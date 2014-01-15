d0d3c1f1123df82777de348bb4e545af4ccf5ff1
Jeremy Morgan <jeremymorgan@gmail.com>
2014-01-14 19:06:38 -0800

    Adding files that show constructor overloading

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-csharpworkshop
+C# Workshop Demo Files
 ==============
 
-C# Workshop Demo Files
+This is a collection of demo files for C# Workshop, my project for teaching C# from start to finish. 
+
+Most of these will be command line code, as most of the lessons use the command line, at least in the beginning. 
+
+The website should be done by Spring 2014.
+
+Some of this will be based on tutorials posted on my [programming blog](http://www.jeremymorgan.com). I will keep updating this as I go.
+
+-- Jeremy
---
+++ Test.cs
@@ -0,0 +1,28 @@
+﻿
+namespace csharplearning
+{
+    class Test
+    {
+        public string ourMessage;
+
+        public test()
+        {
+            ourMessage = "Empty - no parameters";
+        }
+
+        public test(int ourInt)
+        {
+            ourMessage = "An integer was passed";
+        }
+
+        public test(string ourstring)
+        {
+            ourMessage = "A string was passed";
+        }
+
+        public test(string ourstring, int ourInt)
+        {
+            ourMessage = "Both a string and integer were passed";
+        }
+    }
+}
